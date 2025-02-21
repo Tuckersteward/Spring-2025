5e25a96cd1f50f5f5fed0808939daf832261f34d
Cabot Steward <tuckersteward10@gmail.com>
2025-02-21 09:17:07 -0700

    Unit 4 Project and other stuff

--- Data 3300/Python/Unit 2/2-11-25/2-11-25.py
+++ Data 3300/Python/Unit 2/2-11-25/2-11-25.py
@@ -1,1 +1,18 @@
-print('blah')
+'''
+EDA
+    1. Quality issues
+        Issues of accuracy
+        detection of anomolies and outliers
+        understand missing data paterns
+    2. descriptive analysis
+        understand data characteristics
+        understand relationships amongst variables
+        visualize and communicate meaningful data paterns
+    3. fomulate modeling plan
+        form hypotheses
+        select appropriate data types
+'''
+
+'''
+
+'''
